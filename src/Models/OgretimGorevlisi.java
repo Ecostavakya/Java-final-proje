c7bbff308961719d9c5edfcca76f68d5d626a9b9
Ecostavakya <99980948+Ecostavakya@users.noreply.github.com>
2024-01-21 00:34:53 +0300

    Delete src directory

--- src/Models/OgretimGorevlisi.java
+++
@@ -1,8 +0,0 @@
-package Models;
-
-public class OgretimGorevlisi {
-    public static String OgretmenAd;
-    public static String OgretmenSoyad;
-    public static String OgretmenNo;
-    public static String OgretmenDers;
-}
